e3816b5d3713c12a3309c7ec3391045c8a27a609
Francieli Sauthier <francieli.sauthier@gmail>
2023-06-21 15:34:03 +0200

    add mock person data estructure

--- src/components/Patients.jsx
+++ src/components/Patients.jsx
@@ -1,28 +1,8 @@
 import { useState } from 'react';
+import patients from "../assets/patients.json"
 
 export default function Patient() {
   const [isShown, setIsShown] = useState(false);
-
-  const patients = [
-    {
-      name: 'Morpheus',
-      id: '199',
-      createdAt: '2020-02-20T11:00:28.107Z',
-      contactdetails: {
-        phone: '8439743294793',
-        email: 'test@abc.com',
-      },
-    },
-    {
-      name: 'MorpheusII',
-      id: '200',
-      createdAt: '2020-02-20T11:00:28.107Z',
-      contactdetails: {
-        phone: '8439743294793',
-        email: 'test@abc.com',
-      },
-    },
-  ];
 
   const handleClick = (event) => {
     setIsShown(true);
